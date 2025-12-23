03e01fe6bb37a1bee78b2990df999881f3fdce66
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2025-12-23 16:56:36 -0600

    Implemented the third question from practical 1.

---
+++ Computer Programming I/prac_1/print_box.c
@@ -0,0 +1,32 @@
+/* Q4. Write a C program to display the following message. 
+All the white space in the message is created using the space character.
+
+========================
+=                      =
+= She sells sea shells =
+=                      =
+======================== */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    char msg[] = "She sells sea shells";
+
+    for (i = 0; i < 5; i++)
+    {
+        if (i == 0 || i == 4)
+        {
+            printf("========================\n");
+        }
+        else if (i == 2)
+        {
+            printf("= %20s =\n", msg);
+        }
+        else
+        {
+            printf("=                      =\n");
+        }
+    }
+}
